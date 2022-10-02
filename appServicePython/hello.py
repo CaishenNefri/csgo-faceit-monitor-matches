0d61539a904878ee85e2748b976aeff019a188e0
Caishen <24476768+CaishenNefri@users.noreply.github.com>
2022-10-02 23:29:11 +0200

    Create help functions
    Create functions
    - getPlayerTeam to find in which team he played
    - ifTeamWon return boolean if player won match
    - getPlayerMatches - make request to API to get player matches -> not yet working

--- appServicePython/hello.py
+++ appServicePython/hello.py
@@ -1,3 +1,5 @@
+from operator import truediv
+from urllib import response
 import requests
 from flask import Flask
 import json
@@ -9,15 +11,40 @@ def hello():
 
 @myapp.route("/mejz", methods=['GET', 'POST'])
 def parse_request():
+    f = open('mejz_matches.json')
+    response = json.load(f)
+    # for match in response["items"][0]:
+    # match = response["items"][0]
     response = requests.get(
             "https://open.faceit.com/data/v4/players/4ea9d337-ad40-4b55-aab1-0ecf7d5e7dcb/history?game=csgo&offset=0&limit=5",
             headers={"Authorization":"Bearer ***REMOVED***"}
         )
-    for team in response.json()["items"][0]["teams"]:
-        print(team)
-        print(response.json()["items"][0]["teams"][team]["team_id"])
-    # return response.json()["items"][0]["teams"]
+    match = response.json()
+    match = match["items"][0]
+    team = getPlayerTeam(match, "4ea9d337-ad40-4b55-aab1-0ecf7d5e7dcb")
+    print(ifTeamWon(match, team))
     return "OK"
 
-# def get_player_team(match, player):
-    
+def getPlayerTeam(match, player):
+    for team in match["teams"]:
+        print(team)
+        print(match["teams"][team]["team_id"])
+        for gamer in match["teams"][team]["players"]:
+            print(gamer["nickname"])
+            if(player == gamer["player_id"]):
+                return team
+
+def ifTeamWon(match, team):
+    if(match["results"]["winner"] == team):
+        return True
+    else:
+        return False
+
+def getPlayerMatches(player):
+    response = requests.get(
+            "https://open.faceit.com/data/v4/players/{player}/history?game=csgo&offset=0&limit=5",
+            headers={"Authorization":"Bearer ***REMOVED***"}
+        )
+    print("test")
+    print(response.json())
+    return response.json()
