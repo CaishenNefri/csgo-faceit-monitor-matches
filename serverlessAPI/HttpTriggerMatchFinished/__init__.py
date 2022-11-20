04836277f6047928ad3b16be73a5028904ca4942
Caishen <24476768+CaishenNefri@users.noreply.github.com>
2022-11-20 22:36:40 +0100

    Tweak deploy

--- serverlessAPI/HttpTriggerMatchFinished/__init__.py
+++ serverlessAPI/HttpTriggerMatchFinished/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 import azure.functions as func
 
-# from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential
 # from azure.storage.blob import BlobServiceClient
 from azure.data.tables import TableServiceClient
 
@@ -10,7 +10,7 @@ from azure.data.tables import TableServiceClient
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    Acquire a credential object
+    # Acquire a credential object
     credential = DefaultAzureCredential()
     table_service_client = TableServiceClient(
         endpoint="https://storage69415.table.core.windows.net",
